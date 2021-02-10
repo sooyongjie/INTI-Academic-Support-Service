d72bd2ad919863ae8002e5a5c894505e08232760
Soo Yong Jie <sooyongjie@gmail.com>
2021-02-10 14:24:30 +0800

    Dark mode implementation

--- student/js/script.js
+++ student/js/script.js
@@ -4,3 +4,4 @@ window.addEventListener("load", function () {
     document.querySelector(".loading-screen").remove();
   }, 400);
 });
+
--- student/js/theme.js
+++ student/js/theme.js
@@ -6,18 +6,45 @@ window
   });
 
 checkPrefferedTheme = () => {
-  if (localStorage.getItem("theme")) {
-    console.log(
-      'localStorage.getItem("theme"): ',
-      localStorage.getItem("theme")
-    );
-  } else if (
-    window.matchMedia &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches
+  if (
+    !localStorage.getItem("theme") ||
+    (window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches)
   ) {
     localStorage.setItem("theme", "dark");
   } else localStorage.setItem("theme", "light");
-  console.log('localStorage.getItem("theme"): ', localStorage.getItem("theme"));
+  setTheme(localStorage.getItem("theme"));
 };
 
-checkPrefferedTheme();
+document.getElementById("theme-btn").addEventListener("click", () => {
+  if (localStorage.getItem("theme") == "light") {
+    localStorage.setItem("theme", "dark");
+    setTheme("dark");
+  } else {
+    localStorage.setItem("theme", "light");
+    setTheme("light");
+  }
+  console.log(`Loading ${localStorage.getItem("theme")} theme...`);
+  document.body.classList.toggle("dark");
+});
+
+setTheme = (theme) => {
+  if (theme == "light") {
+    linkList[3].href = "../css/student/student.css";
+    document.querySelector(".inti-logo").src = "../img/inti-logo.png";
+  } else {
+    linkList[3].href = "../css/student/student-dark.css";
+    document.querySelector(".inti-logo").src = "../img/inti-logo-dark.png";
+  }
+  console.log(`Loading ${theme} theme...`);
+  document.body.classList.toggle("dark");
+};
+
+const links = document.getElementsByTagName("link");
+const linkList = Array.prototype.slice.call(links);
+
+if (!localStorage.getItem("theme")) {
+  checkPrefferedTheme();
+} else {
+  setTheme(localStorage.getItem("theme"));
+}
