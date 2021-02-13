cd0207cb8c0c841bf7ccb1982a5f054ea4978e82
Soo Yong Jie <sooyongjie@gmail.com>
2021-02-13 14:35:07 +0800

    Updated hyperlinks

--- student/profile.php
+++ student/profile.php
@@ -17,12 +17,6 @@
 <body>
   <?php include_once('./components/nav.php') ?>
   <?php include_once('./components/spinner.php') ?>
-
-  <div class="loading-screen">
-    <svg class="spinner" viewBox="0 0 50 50">
-      <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
-    </svg>
-  </div>
   <div class="container">
     <a class="card">
       <i class="fas fa-sticky-note"></i>
--- student/requests.php
+++ student/requests.php
@@ -16,13 +16,7 @@
 
 <body>
     <?php include_once('./components/nav.php') ?>
-  <?php include_once('./components/spinner.php') ?>
-
-    <div class="loading-screen">
-        <svg class="spinner" viewBox="0 0 50 50">
-            <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
-        </svg>
-    </div>
+    <?php include_once('./components/spinner.php') ?>
     <div class="container row">
         <div class="card request">
             <div class="heading">
@@ -35,7 +29,7 @@
             <label for="">Status</label>
             <div class="col">
                 <p>Pending</p>
-                <a href="./request-view.html" class="view-request">
+                <a href="./request-view.php" class="view-request">
                     <i class="fas fa-arrow-right"></i>
                 </a>
             </div>
@@ -49,7 +43,7 @@
             <label for="">Status</label>
             <div class="col">
                 <p>Pending</p>
-                <a href="./request-view.html" class="view-request">
+                <a href="./request-view.php" class="view-request">
                     <i class="fas fa-arrow-right"></i>
                 </a>
             </div>
