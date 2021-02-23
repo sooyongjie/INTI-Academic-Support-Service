c7ff7522ef3e021d7443109f9ab37b0b87fa4716
Soo Yong Jie <sooyongjie@gmail.com>
2021-02-23 21:39:16 +0800

    Added main javascript file

---
+++ admin/js/script.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function () {
+  document.querySelector(".loading-screen").style.opacity = 0;
+  setTimeout(() => {
+    document.querySelector(".loading-screen").remove();
+  }, 50);
+});
