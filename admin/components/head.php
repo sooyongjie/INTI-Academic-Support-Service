8df4c32120ae4eb8a3bcf9db9cfc7f03c4b5d331
Soo Yong Jie <sooyongjie@gmail.com>
2021-02-23 15:47:06 +0800

    Seperated commonly used head tags to seperate component

---
+++ admin/components/head.php
@@ -0,0 +1,24 @@
+<!-- Primary Meta Tags -->
+<meta charset="UTF-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<meta name="title" content="INTI Academic Services — Purchase Course Structure">
+<meta name="description" content="⚒ Site Under Construction ⚒">
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website">
+<meta property="og:url" content="https://intiacademicsupportservices.000webhostapp.com/">
+<meta property="og:title" content="INTI Academic Services — Purchase Course Structure">
+<meta property="og:description" content="⚒ Site Under Construction ⚒">
+<meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image">
+<meta property="twitter:url" content="https://intiacademicsupportservices.000webhostapp.com/">
+<meta property="twitter:title" content="INTI Academic Services — Purchase Course Structure">
+<meta property="twitter:description" content="⚒ Site Under Construction ⚒">
+<meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png">
+
+<!-- Styling -->
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+<link rel="stylesheet" href="../css/admin/admin.css" />
+<link rel="stylesheet" href="../css/style.css" />
