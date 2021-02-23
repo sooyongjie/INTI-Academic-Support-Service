f7523e27ccc0202d5d391a2b5de84575da1e77b0
Soo Yong Jie <sooyongjie@gmail.com>
2021-02-23 21:43:05 +0800

    Loading spinner implementation

--- admin/components/head.php
+++ admin/components/head.php
@@ -27,3 +27,5 @@
 <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
 <link rel="stylesheet" href="../css/admin/admin.css" />
 <link rel="stylesheet" href="../css/style.css" />
+
+<script src="./js/script.js"></script>
