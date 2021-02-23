83de5ea8a6e5560b8ed8fdc18c9a4c09beb26197
Soo Yong Jie <sooyongjie@gmail.com>
2021-02-23 15:48:42 +0800

    Updated pages to include head component

--- admin/notifications.php
+++ admin/notifications.php
@@ -2,13 +2,8 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Document</title>
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap" rel="stylesheet" />
-  <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
-  <link rel="stylesheet" href="../css/admin/admin.css" />
-  <link rel="stylesheet" href="../css/style.css" />
+<?php include_once('./components/head.php') ?>
+  <title>Notifications</title>
 </head>
 
 <body>
