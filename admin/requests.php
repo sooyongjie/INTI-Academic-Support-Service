ff3a6e75d158e876b93e670809c9750218887ad9
Soo Yong Jie <sooyongjie@gmail.com>
2021-02-25 12:46:08 +0800

    Minor changes

--- admin/requests.php
+++ admin/requests.php
@@ -14,41 +14,40 @@
     <div class="content-container">
       <?php include_once('./components/options.php') ?>
       <?php
-      if (isset($_GET['id'])) { ?>
+      if (!isset($_GET['id'])) { ?>
+
+        <!-- Pending Requests  -->
         <div class="section">
-          <!-- Request  -->
-          <div class="heading">
-            <h2>Request <?php  echo $_GET['id']?></h2>
-          </div>
-          <div class="card request-list">
-            <?php
-            echo "Request details"
-            ?>
-          </div>
-        </div>
-      <?php } else { ?>
-        <div class="section">
-          <!-- Pending Requests  -->
           <div class="heading">
             <h2>Pending Requests</h2>
           </div>
           <div class="card request-list">
-            <?php
-            pendingRequests();
-            ?>
+            <?php pendingRequests() ?>
           </div>
         </div>
+
+        <!-- All Requests  -->
         <div class="section">
-          <!-- All Requests  -->
           <div class="heading">
             <h2>All Requests</h2>
           </div>
           <div class="card request-list">
-            <?php
-            allRequests();
-            ?>
+            <?php allRequests() ?>
           </div>
         </div>
+
+      <?php } else { ?>
+
+        <!-- Request Details  -->
+        <div class="section">
+          <div class="heading">
+            <h2>Request <?php echo $_GET['id'] ?></h2>
+          </div>
+          <div class="card request-list">
+            <?php echo "Request details" ?>
+          </div>
+        </div>
+
       <?php }
       ?>
     </div>
