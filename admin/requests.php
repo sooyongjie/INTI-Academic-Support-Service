d5433f777ed913a33de5eeff4898c30475cc8b20
Soo Yong Jie <sooyongjie@gmail.com>
2021-04-30 16:20:58 +0800

    Minor fix and changes

--- admin/requests.php
+++ admin/requests.php
@@ -24,40 +24,12 @@
             <?php pendingRequests() ?>
           </div>
           <!-- Pagination -->
-          <input type="hidden" value="<?php echo $_SESSION['offset'] ?>" name="offset" id="offset">
-          <input type="hidden" value="<?php echo $_SESSION['limit'] ?>" name="limit" id="limit">
-          <form class="pagination">
-            <?php
-            if ($_SESSION['offset'] != 0 && $_SESSION['offset'] % $_SESSION['limit'] == 0) {
-            } else {
-            } ?>
-            <?php
-            if ($_SESSION['page'] == 1) { ?>
-              <button class="pagination-disabled">
-                <i class="fas fa-angle-left"></i>
-              </button>
-            <?php } else { ?>
-              <button type="button" onclick="minusOffset()">
-                <i class="fas fa-angle-left pagination-enabled"></i>
-              </button>
-            <?php } ?>
-            <input type="number" value="<?php echo $_SESSION['page'] ?>" name="page" id="page-input">
-            <?php
-            if ($_SESSION['page'] >= $_SESSION['totalPages']) { ?>
-              <button type="button" class="pagination-disabled">
-                <i class="fas fa-angle-right"></i>
-              </button>
-            <?php } else { ?>
-              <button type="button" onclick="addOffset()">
-                <i class="fas fa-angle-right pagination-enabled"></i>
-              </button>
-            <?php } ?>
-          </form>
+          <?php include_once('./components/pagination.php') ?>
         </div>
         <!-- All Requests  -->
         <div class="section">
           <div class="heading">
-            <!-- <h2>All Requests</h2> -->
+            <h2>All Requests</h2>
           </div>
         </div>
       <?php
