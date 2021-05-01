c9478a47dc11f05ffa7c24eef449a1eb0241f79b
Soo Yong Jie <sooyongjie@gmail.com>
2021-05-01 15:19:11 +0800

    Minor changes

--- admin/requests-all.php
+++ admin/requests-all.php
@@ -13,22 +13,17 @@
     <div class="container">
         <div class="content-container">
             <?php include_once('./components/options.php') ?>
-            <?php
-            if (!isset($_GET['id'])) { ?>
-                <!-- Pending Requests  -->
-                <div class="section">
-                    <div class="heading">
-                        <h2>All Requests</h2>
-                    </div>
-                    <div class="card request-list">
-                        <?php allRequests() ?>
-                    </div>
-                    <!-- Pagination -->
-                    <?php include_once('./components/pagination.php') ?>
+            <!-- All Requests  -->
+            <div class="section">
+                <div class="heading">
+                    <h2>All Requests</h2>
                 </div>
-            <?php
-            }
-            ?>
+                <div class="card request-list">
+                    <?php allRequests() ?>
+                </div>
+                <!-- Pagination -->
+                <?php include_once('./components/pagination.php') ?>
+            </div>
         </div>
     </div>
     <?php if (isset($_SESSION['toast'])) include_once('./components/toast.php') ?>
