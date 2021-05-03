ee4eaf3428fd84302224003fb7a892d5495749d4
Soo Yong Jie <sooyongjie@gmail.com>
2021-05-03 14:51:19 +0800

    Bug fix

--- student/func/payment.php
+++ student/func/payment.php
@@ -8,7 +8,11 @@ uploadPayment($_POST['reqID'], $_POST['token']);
 function uploadPayment($reqID, $token)
 {
     echo $reqID . " " . $token;
+    // update payment record
     $query = "UPDATE `payment` SET `token`= '$token' WHERE `reqID` = $reqID";
+    $result = updateQuery($query);
+    // update payment ID in request record
+    $query = "UPDATE `request` SET `payID`= '$reqID' WHERE `reqID` = $reqID";
     $result = updateQuery($query);
     $header = "Location: ../request-view.php?id=" . $reqID;
     if ($result) header($header);
