24e02444a6124053e4ab191a11f42ad3d687b872
Soo Yong Jie <sooyongjie@gmail.com>
2021-05-11 15:44:28 +0800

    Request status can now be updated

--- admin/func/status.php
+++ admin/func/status.php
@@ -1,9 +1,17 @@
 <?php
 
+require_once('./func.php');
+require("../../db_connect.php");
+
+
 if (isset($_GET['approve'])) updateStatus($_GET['approve'], 2);
 else if (isset($_GET['cancel'])) updateStatus($_GET['cancel'], 0);
 
 function updateStatus($id, $status)
-{
+{   
     $query = "UPDATE `requests` SET `status` = $status WHERE `reqID` = $id ";
+    $result = updateQuery($query);
+    $header = "Location: ../requests.php?id=" . $id;
+    if ($result) header($header);
+
 }
